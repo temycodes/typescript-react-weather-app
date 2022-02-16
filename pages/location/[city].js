ba040f1373d10e672c13b935113d088accd238cf
Heytemisan <temyicy@gmail.com>
2022-02-16 19:44:08 +0100

    fix: code clean up

--- pages/location/[city].js
+++ pages/location/[city].js
@@ -5,7 +5,7 @@ export async function getServerSideProps(context) {
   //center call to grab information`city`
   const city = getCity(context.params.city);
   const slug = context.params.city;
-  
+  //check
   if (!city) {
     return {
       notFound: true,
@@ -24,12 +24,13 @@ const getCity = (param) => {
   const cityParam = param.trim();
   //get the city id ~ split params with a dash -
   const splitCity = cityParam.split("-");
-  //grab the last array element of splitCity
+  //grab the last array element of splited city
   const id = splitCity[splitCity.length - 1];
+  //if no date is specified
   if (!id) {
       return null;
-  }
-
+  } 
+  //connect json file
   const city = cities.find(city => city.id.toString() == id);
   if (city) {
     return city;
