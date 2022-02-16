dce90152e9913b1a762a13ae38d5147e81fa2231
Heytemisan <temyicy@gmail.com>
2022-02-16 15:17:28 +0100

    feat: created city params side section

--- pages/location/[city].js
+++ pages/location/[city].js
@@ -1,13 +1,46 @@
 import React from 'react';
+import cities from '../../lib/city.list.json'
 
 export async function getServerSideProps(context) {
-  return {
-    
+  const city = getCity(context.params.city);
+  const slug = context.params.city;
+  
+  if (!city) {
+    return {
+      notFound: true,
+    };
   }
+
+  return {
+    props: {
+      slug: slug
+    },
+  };
 }
 
-export default function City() {
+const getCity = (param) => {
+  const cityParam = param.trim();
+  //get the city id
+  const splitCity = cityParam.split("-");
+  //grab the last array element of splitCity
+  const id = splitCity[splitCity.length - 1];
+  if (!id) {
+      return null;
+  }
+
+  const city = cities.find(city => city.id.toString() == id);
+  if (city) {
+    return city;
+  } else {
+    return null;
+  }
+};
+
+export default function City({slug}) {
   return (
-    <div>City Page</div>
+    <div>
+      <h1>City Page</h1>
+      <h2>{slug}</h2>
+    </div>
   )
 }
